cea4a6973f6027ef2326ac0fe88087f07a37d54f
Firat Seven <Firatsevmeyen@gmail.com>
2021-09-01 15:10:09 +0300

    RecapDemo2

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -18,9 +18,45 @@ namespace Business.Concrete
             _carDal = carDal;
         }
 
+        public void Add(Car car)
+        {
+            if(car.Description.Length>2 && car.DailyPrice > 0)
+            {
+                _carDal.Add(car);
+                Console.WriteLine("New Car Added: {0}\n", car.Description);
+            }
+            else
+            {
+                Console.WriteLine("There is an error. Car didnt add.Please look your detail.");
+            }
+        }
+
+        public void Delete(Car car)
+        {
+            _carDal.Delete(car);
+            Console.WriteLine("Car has been updated.\n");
+        }
+        public void Update(Car car)
+        {
+            _carDal.Update(car);
+            Console.WriteLine("Car has been updated.\n");
+        }
+
         public List<Car> GetAll()
         {
             return _carDal.GetAll();
         }
+
+        public List<Car> GetCarsByBrandId(int id)
+        {
+            return _carDal.GetAll(c => c.BrandId == id);
+        }
+
+        public List<Car> GetCarsByColorId(int id)
+        {
+            return _carDal.GetAll(c => c.ColorId == id);
+        }
+
+       
     }
 }
--- DataAccess/Concrete/InMemory/InMemoryCarDal.cs
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -3,6 +3,7 @@ using Entities.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -17,16 +18,16 @@ namespace DataAccess.Concrete.InMemory
         {
             _cars = new List<Car>
             {
-                new Car{CarId=1,BrandId=2,ColorId=3,ModelYear=2020,DailyPrice=150000,Description="Spor Araç"},
-                new Car{CarId=2,BrandId=2,ColorId=1,ModelYear=1970,DailyPrice=12000,Description="Audi V7"},
-                new Car{CarId=3,BrandId=1,ColorId=2,ModelYear=2015,DailyPrice=110000,Description="Arazi Araç"},
-                new Car{CarId=4,BrandId=4,ColorId=4,ModelYear=2017,DailyPrice=170000,Description="Spor Araç"},
-                new Car{CarId=5,BrandId=3,ColorId=4,ModelYear=2000,DailyPrice=70000,Description="Yük Aracı"},
-                new Car{CarId=6,BrandId=4,ColorId=2,ModelYear=2022,DailyPrice=650000,Description="Sıfır Araç"},
-                new Car{CarId=7,BrandId=5,ColorId=1,ModelYear=2013,DailyPrice=125000,Description="Ferrari 516"},
-                new Car{CarId=8,BrandId=5,ColorId=4,ModelYear=2018,DailyPrice=180000,Description="Buggatti"},
-                new Car{CarId=9,BrandId=1,ColorId=5,ModelYear=1965,DailyPrice=135000,Description="Mercedes XX"},
-                new Car{CarId=10,BrandId=3,ColorId=3,ModelYear=2022,DailyPrice=750000,Description="Volvo X4"}
+                new Car{Id=1,BrandId=2,ColorId=3,ModelYear=2020,DailyPrice=150000,Description="Spor Araç"},
+                new Car{Id=2,BrandId=2,ColorId=1,ModelYear=1970,DailyPrice=12000,Description="Audi V7"},
+                new Car{Id=3,BrandId=1,ColorId=2,ModelYear=2015,DailyPrice=110000,Description="Arazi Araç"},
+                new Car{Id=4,BrandId=4,ColorId=4,ModelYear=2017,DailyPrice=170000,Description="Spor Araç"},
+                new Car{Id=5,BrandId=3,ColorId=4,ModelYear=2000,DailyPrice=70000,Description="Yük Aracı"},
+                new Car{Id=6,BrandId=4,ColorId=2,ModelYear=2022,DailyPrice=650000,Description="Sıfır Araç"},
+                new Car{Id=7,BrandId=5,ColorId=1,ModelYear=2013,DailyPrice=125000,Description="Ferrari 516"},
+                new Car{Id=8,BrandId=5,ColorId=4,ModelYear=2018,DailyPrice=180000,Description="Buggatti"},
+                new Car{Id=9,BrandId=1,ColorId=5,ModelYear=1965,DailyPrice=135000,Description="Mercedes XX"},
+                new Car{Id=10,BrandId=3,ColorId=3,ModelYear=2022,DailyPrice=750000,Description="Volvo X4"}
             };
         }
         public void Add(Car car)
@@ -36,18 +37,23 @@ namespace DataAccess.Concrete.InMemory
 
         public void Delete(Car car)
         {
-            Car carToDelete = _cars.SingleOrDefault(c => c.CarId == c.CarId);
+            Car carToDelete = _cars.SingleOrDefault(c => c.Id == c.Id);
             _cars.Remove(carToDelete);
         }
 
-        public List<Car> GetAll()
+        public List<Car> GetAll(Expression<Func<Car, bool>> filter = null)
         {
             return _cars;
         }
 
+        public List<Car> GetById(Expression<Func<Car, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+         
         public void Update(Car car)
         {
-            Car carToUpdate = _cars.SingleOrDefault(c => c.CarId == car.CarId);
+            Car carToUpdate = _cars.SingleOrDefault(c => c.Id == car.Id);
             carToUpdate.BrandId = car.BrandId;
             carToUpdate.ColorId = car.ColorId;
             carToUpdate.DailyPrice = car.DailyPrice;
